cc067779f933ae6617114bd3878055e2543cfdbc
Julien Herzen <julien.herzen@unit8.co>
2018-12-28 18:03:31 +0100

    Basic package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vegans',
+      version='0.1',
+      description='A library providing various existing GANs in PyTorch.',
+      url='https://gitlab.com/hrzn/vegans',
+      author='Julien Herzen',
+      author_email='j.herzen@gmail.com',
+      license='MIT',
+      packages=['vegans'],
+      zip_safe=False)
