337cf1cd802485798b31f55829da12fe6da2fe92
Julien Herzen <julien.herzen@unit8.co>
2019-01-02 22:42:26 +0100

    added wgan

--- vegans/gan.py
+++ vegans/gan.py
@@ -10,7 +10,7 @@ class GAN:
         self.nz = nz
 
     def _weights_init(self,):
-        # custom weights initialization called on generator and discriminator
+        # custom weights initialization called on generator and discriminator; based on DCGAN
         for m in [self.generator, self.discriminator]:
             classname = m.__class__.__name__
             if classname.find('Conv') != -1:
@@ -22,8 +22,8 @@ class GAN:
     def train(self,
               dataloader,
               ngpu=0,
-              optimizerD=None,
-              optimizerG=None,
+              optimizer_D=None,
+              optimizer_G=None,
               num_epochs=5,
               fixed_noise_size=64,
               init_weights=True,
@@ -34,8 +34,8 @@ class GAN:
 
         :param dataloader:
         :param ngpu:
-        :param optimizerD:
-        :param optimizerG:
+        :param optimizer_D:
+        :param optimizer_G:
         :param num_epochs:
         :param fixed_latent_batch_size:
         :param init_weights:
@@ -59,26 +59,25 @@ class GAN:
         """ Default optimizers for G and D
             TODO: abstract function?
         """
-        if optimizerD is None:
-            optimizerD = optim.Adam(self.discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-        if optimizerG is None:
-            optimizerG = optim.Adam(self.generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-
+        if optimizer_D is None:
+            optimizer_D = optim.Adam(self.discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+        if optimizer_G is None:
+            optimizer_G = optim.Adam(self.generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
 
         """ Training Loop
         """
-        # Lists to keep track of progress
-        samples_list = []
-        G_losses = []
-        D_losses = []
+        # Structures keep track of progress. <(epoch, mini_batch), value>
+        samples_list = dict()
+        G_losses = dict()
+        D_losses = dict()
         iters = 0
 
         print("Starting training Loop...")
         for epoch in range(num_epochs):
             for i, (data, _) in enumerate(dataloader):
 
-                real_device = data.to(device)
-                batch_size = real_device.size(0)
+                real = data.to(device)
+                batch_size = real.size(0)
                 real_labels = torch.full((batch_size,), 1.0, device=device)
                 fake_labels = torch.full((batch_size,), 0.0, device=device)
 
@@ -86,8 +85,8 @@ class GAN:
                     The BCE loss is: -[y log(x) + (1 - y) log(1 - x)], so we want to minimize it
                 """
                 ## Train with real
-                self.discriminator.zero_grad()
-                output = self.discriminator(real_device)
+                optimizer_D.zero_grad()
+                output = self.discriminator(real)
                 errD_real = criterion(output, real_labels)  # loss on real batch
                 errD_real.backward()  # gradients for real batch
                 D_x = output.mean().item()
@@ -100,18 +99,18 @@ class GAN:
                 errD_fake.backward()  # gradients for fake batch
                 D_G_z1 = output.mean().item()
                 errD = errD_real + errD_fake  # total loss (sum of real and fake losses)
-                optimizerD.step()  # Update D
+                optimizer_D.step()  # Update D
 
                 ############################
                 # (2) Update G network: maximize log(D(G(z)))
                 ###########################
-                self.generator.zero_grad()
+                optimizer_G.zero_grad()
                 # Since we just updated D, perform another forward pass of all-fake batch through D
                 output = self.discriminator(fake).view(-1)
                 errG = criterion(output, real_labels)  # loss. Fake labels are real for generator cost
                 errG.backward()
                 D_G_z2 = output.mean().item()
-                optimizerG.step()  # Update G
+                optimizer_G.step()  # Update G
 
                 # Output training stats
                 if i % print_every == 0:
@@ -119,15 +118,15 @@ class GAN:
                           % (epoch, num_epochs, i, len(dataloader),
                              errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
 
-                G_losses.append(errG.item())
-                D_losses.append(errD.item())
+                G_losses[(epoch, i)] = errG.item()
+                D_losses[(epoch, i)] = errD.item()
 
                 # Check how the generator is doing by saving G's output on fixed_noise
                 if (iters % save_every == 0) or ((epoch == num_epochs - 1) and (i == len(dataloader) - 1)):
                     with torch.no_grad():
                         fake = self.generator(fixed_noise).detach().cpu()
                     # samples_list.append(vutils.make_grid(fake, padding=2, normalize=True))
-                    samples_list.append(fake)
+                    samples_list[(epoch, i)] = fake
                 iters += 1
 
         return samples_list, G_losses, D_losses
