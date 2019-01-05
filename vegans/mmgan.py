c1f87c89305f661bad7b17d9949f97079c7f6bf8
Julien Herzen <julien.herzen@unit8.co>
2019-01-05 12:08:22 +0100

    Add an empty line

--- vegans/mmgan.py
+++ vegans/mmgan.py
@@ -7,7 +7,7 @@ class MMGAN(GAN):
     """
     Minimax GAN (classic GAN), in its non-saturated version (i.e., generator loss is log(D(x)) ).
     Also called NS-GAN sometimes.
-    
+
     https://arxiv.org/abs/1406.2661
     """
 
