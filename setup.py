85d885f05447889e46f4b0ef923316006c44b71f
Julien Herzen <julien.herzen@unit8.co>
2019-01-24 22:02:57 +0100

    Updated site in setup file

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='vegans',
       version='0.1',
       description='A library providing various existing GANs in PyTorch.',
-      url='https://gitlab.com/hrzn/vegans',
+      url='https://github.com/unit8co/vegans/',
       author='Julien Herzen',
-      author_email='j.herzen@gmail.com',
+      author_email='julien@unit8.co',
       license='MIT',
       packages=['vegans'],
       zip_safe=False)
