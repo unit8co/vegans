70a2c45f728443cb27193269214bb1307494c4de
Julien Herzen <julien.herzen@unit8.co>
2019-01-29 22:00:52 +0100

    Read readme in setup

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), 'r') as f:
+    long_description = f.read()
 
 setup(name='vegans',
       version='0.1.0',
       description='A library to easily train various existing GANs in PyTorch.',
-      long_description=read('README.md'),
+      long_description=long_description,
       keywords='gan gans pytorch generative adversarial networks',
       url='https://github.com/unit8co/vegans/',
       author='Unit8',
