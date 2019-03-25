6c91b3eadc4d2d53c44541acd37201cf88d0910b
Radu <radu@unit8.co>
2019-03-25 16:52:51 +0100

    combine tests

--- vegans/tests/test_integration.py
+++ vegans/tests/test_integration.py
@@ -5,18 +5,14 @@ from .conftest import nz
 
 
 class TestIntegration(object):
+    @pytest.mark.parametrize('device', [
+        'cpu',
+        'cuda'
+    ])
     @pytest.mark.parametrize('nn', [
         MMGAN,
         WGAN,
         WGANGP,
     ])
-    def test_run_cpu(self, nn, generator, critic, gaussian_dataloader):
-        nn(generator, critic, gaussian_dataloader, nz=nz, device='cpu').train()
-
-    @pytest.mark.parametrize('nn', [
-        MMGAN,
-        WGAN,
-        WGANGP,
-    ])
-    def test_run_gpu(self, nn, generator, critic, gaussian_dataloader):
-        nn(generator, critic, gaussian_dataloader, nz=nz, device='cuda').train()
+    def test_run(self, device, nn, generator, critic, gaussian_dataloader):
+        nn(generator, critic, gaussian_dataloader, nz=nz, device=device).train()
