7055fba0a0621120b4e0c5bdf1dc260afaf4b352
Radu <radu@unit8.co>
2019-05-06 13:05:02 +0200

    Make script windows-friendly

--- examples/create_gif.sh
+++ examples/create_gif.sh
@@ -10,15 +10,15 @@ cd ../resources
 rm -rf combined.gif                                                 # clean previous
 
 # per-gif
-convert wgan_gp.gif -gravity West -chop 50x0 -gravity East -chop 50x0 wgan_gp.gif   # cut empty sides
-convert began.gif -gravity West -chop 50x0 -gravity East -chop 50x0 began.gif       # cut empty sides
-convert wgan_gp.gif -coalesce a-%04d.gif                            # separate frames of wgan_gp.gif
-convert began.gif -coalesce b-%04d.gif                              # separate frames of began.gif
+magick convert wgan_gp.gif -gravity West -chop 50x0 -gravity East -chop 50x0 wgan_gp.gif   # cut empty sides
+magick convert began.gif -gravity West -chop 50x0 -gravity East -chop 50x0 began.gif       # cut empty sides
+magick convert wgan_gp.gif -coalesce a-%04d.gif                            # separate frames of wgan_gp.gif
+magick convert began.gif -coalesce b-%04d.gif                              # separate frames of began.gif
 
 # combine
-for f in a-*.gif; do convert ${f} ${f/a/b} +append ${f}; done       # append frames side-by-side
-convert -loop 0 -delay 60 a-*.gif combined.gif                      # rejoin frames
+for f in a-*.gif; do magick convert ${f} ${f/a/b} +append ${f}; done       # append frames side-by-side
+magick convert -loop 0 -delay 60 a-*.gif combined.gif                      # rejoin frames
 
 # post
-rm -rf a-* b-*                                                      # cleanup
-convert combined.gif -fuzz 10% -layers Optimize combined.gif        # reduce size
+rm -rf a-* b-*                                                             # cleanup
+magick convert combined.gif -fuzz 10% -layers Optimize combined.gif        # reduce size
