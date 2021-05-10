0f005e8e9ff39e59e7f24439f136d30855e30409
Thomas Neuer <neuer.thom@gmail.com>
2021-05-10 17:13:42 +0200

    Better dataloading for all datasets

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -5,11 +5,44 @@ import numpy as np
 import vegans.utils.utils as utils
 import vegans.utils.loading as loading
 
-def test_MNISTLoader():
 
+def test_MNISTLoader():
     loader = loading.MNISTLoader()
     X_train, y_train, X_test, y_test = loader.load()
     assert X_train.shape == (60000, 1, 32, 32)
     assert y_train.shape == (60000, 10)
     assert X_test.shape == (10000, 1, 32, 32)
     assert y_test.shape == (10000, 10)
+
+def test_FashionMNISTLoader():
+    loader = loading.FashionMNISTLoader()
+    X_train, y_train, X_test, y_test = loader.load()
+    assert X_train.shape == (60000, 1, 32, 32)
+    assert y_train.shape == (60000, 10)
+    assert X_test.shape == (10000, 1, 32, 32)
+    assert y_test.shape == (10000, 10)
+
+def test_Cifar10Loader():
+    loader = loading.CIFAR10Loader()
+    X_train, y_train, X_test, y_test = loader.load()
+    assert X_train.shape == (50000, 3, 32, 32)
+    assert y_train.shape == (50000, 10)
+    assert X_test.shape == (10000, 3, 32, 32)
+    assert y_test.shape == (10000, 10)
+
+def test_Cifar100Loader():
+    loader = loading.CIFAR100Loader()
+    X_train, y_train, X_test, y_test = loader.load()
+    assert X_train.shape == (50000, 3, 32, 32)
+    assert y_train.shape == (50000, 100)
+    assert X_test.shape == (10000, 3, 32, 32)
+    assert y_test.shape == (10000, 100)
+
+def test_CelebALoader():
+    loader = loading.CelebALoader()
+    train_loader = loader.load()
+    X_train, y_train = iter(train_loader).__next__()
+    X_train, y_train = X_train.numpy(), y_train.numpy()
+    assert X_train.shape == (32, 3, 218, 178)
+    assert y_train.shape == (32, 40)
+
--- vegans/utils/loading.py
+++ vegans/utils/loading.py
@@ -6,16 +6,21 @@ import subprocess
 import torchvision
 
 import numpy as np
+import pandas as pd
+
+from PIL import Image
 from pathlib import Path
 from abc import ABC, abstractmethod
+from torch.utils.data import DataLoader
+from vegans.utils.utils import invert_channel_order
 
 import vegans.utils.architectures as architectures
 
 class DatasetMetaData():
-    def __init__(self, directory, uris, python_hashes):
+    def __init__(self, directory, uris, m5hashes):
         self.directory = directory
         self.uris = uris
-        self.python_hashes = python_hashes
+        self.m5hashes = m5hashes
 
 class DatasetLoader(ABC):
     """
@@ -53,9 +58,6 @@ class DatasetLoader(ABC):
 
     @abstractmethod
     def _download_dataset(self):
-        """
-        Downloads the dataset in the root directory
-        """
         os.makedirs(self._root, exist_ok=True)
 
     def _get_path_dataset(self) -> Path:
@@ -74,13 +76,6 @@ class DatasetLoader(ABC):
         """
         Given a Path to the file and a DataLoaderMetadata object, returns train and test sets as numpy arrays.
         One can assume that the file exists and its MD5 checksum has been verified before this function is called
-
-        Parameters
-        ----------
-        path_to_file: Path
-            A Path object where the dataset is located
-        metadata: Metadata
-            The dataset's metadata
         """
         pass
 
@@ -145,15 +140,17 @@ class MNISTLoader(DatasetLoader):
         self.x_dim = x_dim
         self.z_dim = z_dim
         self.y_dim = y_dim
+        self.path_data = "mnist_data.pickle"
+        self.path_targets = "mnist_targets.pickle"
         uris = {
             "data": "",
             "targets": ""
         }
-        python_hashes = {
+        m5hashes = {
             "data": "9e7c1694ff8fa70086505beb76ee1bda",
             "targets": "06915ca44ac91e0fa65792d391bec292"
         }
-        metadata = DatasetMetaData(directory="MNIST", uris=uris, python_hashes=python_hashes)
+        metadata = DatasetMetaData(directory="MNIST", uris=uris, m5hashes=m5hashes)
         super().__init__(metadata=metadata, root=root)
 
     def _download_dataset(self):
@@ -161,32 +158,35 @@ class MNISTLoader(DatasetLoader):
         for key, uri in self._metadata.uris.items():
             try:
                 request = requests.get(uri)
+                path = os.path.join(self.path, key)
                 with open(self.path, "wb") as f:
                     f.write(request.content)
             except Exception as e:
                 raise ValueError("Could not download the dataset. Reason :" + e.__repr__()) from None
 
     def _load_from_disk(self):
-        path = os.path.join(self.path, "mnist_data.pickle")
-        with open(path, "rb") as f:
-            data = pickle.load(f)
-            self._check_dataset_integrity_or_raise(path=path, expected_hash=self._metadata.python_hashes["data"])
-            X_train, X_test = data["train"], data["test"]
+        X_train, X_test = self._load_from_path(
+            path=os.path.join(self.path, self.path_data), m5hash=self._metadata.m5hashes["data"]
+        )
 
-        path = os.path.join(self.path, "mnist_targets.pickle")
         if self.y_dim is not None:
-            with open(os.path.join(self.path, "mnist_targets.pickle"), "rb") as f:
-                targets = pickle.load(f)
-                self._check_dataset_integrity_or_raise(path=path, expected_hash=self._metadata.python_hashes["targets"])
-                y_train, y_test = targets["train"], targets["test"]
+            y_train, y_test = self._load_from_path(
+                path=os.path.join(self.path, self.path_targets), m5hash=self._metadata.m5hashes["targets"]
+            )
         else:
-            y_train = None
-            y_test = None
+            y_train = y_test = None
 
         X_train, y_train, X_test, y_test = self._preprocess(X_train, y_train, X_test, y_test)
         if y_train is None:
             return X_train, X_test
         return X_train, y_train, X_test, y_test
+
+    def _load_from_path(self, path, m5hash):
+        with open(path, "rb") as f:
+            data = pickle.load(f)
+            self._check_dataset_integrity_or_raise(path=path, expected_hash=m5hash)
+            train, test = data["train"], data["test"]
+        return train, test
 
     @staticmethod
     def _preprocess(X_train, y_train, X_test, y_test):
@@ -219,3 +219,206 @@ class MNISTLoader(DatasetLoader):
     def load_decoder(self):
         return architectures.load_mnist_decoder(z_dim=self.z_dim, y_dim=self.ydim)
 
+
+class FashionMNISTLoader(MNISTLoader):
+    def __init__(self, x_dim=(1, 32, 32), z_dim=(64, ), y_dim=(10, ), root=None):
+        self.x_dim = x_dim
+        self.z_dim = z_dim
+        self.y_dim = y_dim
+        self.path_data = "fashionmnist_data.pickle"
+        self.path_targets = "fashionmnist_targets.pickle"
+        uris = {
+            "data": "",
+            "targets": ""
+        }
+        m5hashes = {
+            "data": "a25612811c69618cdb9f3111446285f4",
+            "targets": "a85af1a3c426f56c52911c7a1cfe5b19"
+        }
+        metadata = DatasetMetaData(directory="FashionMNIST", uris=uris, m5hashes=m5hashes)
+        DatasetLoader.__init__(self, metadata=metadata, root=root)
+
+
+class CIFAR10Loader(MNISTLoader):
+    def __init__(self, x_dim=(3, 32, 32), z_dim=(64, ), y_dim=(10, ), root=None):
+        self.x_dim = x_dim
+        self.z_dim = z_dim
+        self.y_dim = y_dim
+        self.path_data = "cifar10_data.pickle"
+        self.path_targets = "cifar10_targets.pickle"
+        uris = {
+            "data": "",
+            "targets": ""
+        }
+        m5hashes = {
+            "data": "40e8e2ca6c43feaa1c7c78a9982b978e",
+            "targets": "9a7e604de1826613e860e0bce5a6c1d0"
+        }
+        metadata = DatasetMetaData(directory="CIFAR10", uris=uris, m5hashes=m5hashes)
+        DatasetLoader.__init__(self, metadata=metadata, root=root)
+
+    @staticmethod
+    def _preprocess(X_train, y_train, X_test, y_test):
+        """ Preprocess mnist by normalizing and padding.
+        """
+        max_number = X_train.max()
+        X_train = X_train / max_number
+        X_test = X_test / max_number
+
+        if y_train is not None:
+            y_train = np.eye(10)[y_train.reshape(-1)]
+            y_test = np.eye(10)[y_test.reshape(-1)]
+        return X_train, y_train, X_test, y_test
+
+
+class CIFAR100Loader(MNISTLoader):
+    def __init__(self, x_dim=(3, 32, 32), z_dim=(64, ), y_dim=(10, ), root=None):
+        self.x_dim = x_dim
+        self.z_dim = z_dim
+        self.y_dim = y_dim
+        self.path_data = "cifar100_data.pickle"
+        self.path_targets = "cifar100_targets.pickle"
+        uris = {
+            "data": "",
+            "targets": ""
+        }
+        m5hashes = {
+            "data": "d0fc36fde6df99d13fc8d9b20a87bd37",
+            "targets": "48495792f9c4d719b84b56127d4d725a"
+        }
+        metadata = DatasetMetaData(directory="CIFAR100", uris=uris, m5hashes=m5hashes)
+        DatasetLoader.__init__(self, metadata=metadata, root=root)
+
+    @staticmethod
+    def _preprocess(X_train, y_train, X_test, y_test):
+        """ Preprocess mnist by normalizing and padding.
+        """
+        max_number = X_train.max()
+        X_train = X_train / max_number
+        X_test = X_test / max_number
+
+        if y_train is not None:
+            y_train = np.eye(100)[y_train.reshape(-1)]
+            y_test = np.eye(100)[y_test.reshape(-1)]
+        return X_train, y_train, X_test, y_test
+
+
+class CelebALoader(DatasetLoader):
+    def __init__(self, x_dim=(3, 218, 178), z_dim=(128, ), y_dim=(10, ), root=None, batch_size=32, max_loaded_images=5000, **kwargs):
+        """
+        Parameters
+        ----------
+        batch_size : int
+            batch size during training.
+        max_loaded_images : int
+            Number of examples loaded into memory, before new batch is loaded.
+        kwargs
+            Other input arguments to torchvision.utils.data.DataLoader
+        """
+        self.x_dim = x_dim
+        self.z_dim = z_dim
+        self.y_dim = y_dim
+        self.batch_size = batch_size
+        self.max_loaded_images = max_loaded_images
+        self.kwargs = kwargs
+        uris = {
+            "data": ""
+        }
+        m5hashes = {
+            "targets": "55dfc34188defde688032331b34f9286"
+        }
+        metadata = DatasetMetaData(directory="CelebA", uris=uris, m5hashes=m5hashes)
+        DatasetLoader.__init__(self, metadata=metadata, root=root)
+
+    def _download_dataset(self):
+        super()._download_dataset()
+        os.mkdir(os.path.join(self._root, "CelebA"))
+        for key, uri in self._metadata.uris.items():
+            try:
+                request = requests.get(uri)
+                path = os.path.join(self.path, key)
+                with open(path, "wb") as f:
+                    f.write(request.content)
+            except Exception as e:
+                raise ValueError("Could not download the dataset. Reason :" + e.__repr__()) from None
+
+    def _load_from_disk(self):
+        class DataSet():
+            def __init__(self, root, max_loaded_images, load_attributes):
+                self.root = root
+                self.datapath = os.path.join(root, "CelebA/images/")
+                self.attributepath = os.path.join(root, "CelebA/list_attr_celeba.csv")
+                self.nr_samples = 202599
+                self.max_loaded_images = max_loaded_images
+                self.image_shape = (3, 218, 178)
+                self.load_attributes = load_attributes
+                try:
+                    self.image_names = os.listdir(self.datapath)
+                except FileNotFoundError:
+                    raise FileNotFoundError(
+                        "No such file or directory: '{}'. Download from: https://www.kaggle.com/jessicali9530/celeba-dataset."
+                        .format(self.datapath)
+                    )
+                self.current_batch = -1
+                self._numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+
+            def __len__(self):
+                return self.nr_samples
+
+            def __getitem__(self, index):
+                this_batch = index // self.max_loaded_images
+                if this_batch != self.current_batch:
+                    self.current_batch = this_batch
+                    self.images, self.attributes = self._load_data(start=index)
+                index = index % self.max_loaded_images
+
+                if self.attributes is None:
+                    self.images[index]
+                return self.images[index], self.attributes[index]
+
+            def _load_data(self, start):
+                end = start + self.max_loaded_images
+
+                if self.load_attributes:
+                    attributes = pd.read_csv(self.attributepath).iloc[start:start+end, :]
+                    attributes = self._transform_targets(targets=attributes)
+                else:
+                    attributes = None
+
+                batch_image_names = self.image_names[start:end]
+                images = np.array([np.array(Image.open(self.datapath+im_name)) for im_name in batch_image_names])
+                images = self._transform_data(data=images)
+                return images, attributes
+
+            def _transform_targets(self, targets):
+                targets = targets.select_dtypes(include=self._numerics).values
+                return targets
+
+            def _transform_data(self, data):
+                data = invert_channel_order(images=data)
+                return data.reshape((-1, *self.image_shape)) / 255
+
+        self._check_dataset_integrity_or_raise(
+            path=os.path.join(self._root, "CelebA/list_attr_celeba.csv"), expected_hash=self._metadata.m5hashes["targets"]
+        )
+        train_dataloader = DataLoader(DataSet(
+                root=self._root, max_loaded_images=self.max_loaded_images, load_attributes=self.y_dim is not None
+            ), batch_size=self.batch_size, **self.kwargs
+        )
+
+        return train_dataloader
+
+    def load_generator(self):
+        return architectures.load_mnist_generator(z_dim=self.z_dim, y_dim=self.ydim)
+
+    def load_adversary(self, adv_type):
+        return architectures.load_mnist_adversary(adv_type=adv_type, y_dim=self.ydim)
+
+    def load_encoder(self):
+        return architectures.load_mnist_encoder(x_dim=self.x_dim, z_dim=self.z_dim, y_dim=self.ydim)
+
+    def load_autoencoder(self):
+        return architectures.load_mnist_autoencoder(z_dim=self.z_dim, y_dim=self.ydim)
+
+    def load_decoder(self):
+        return architectures.load_mnist_decoder(z_dim=self.z_dim, y_dim=self.ydim)
